938cdf5f029614b57fb4e39f4f30a8a044b5042d
luthor628 <abdulrahmanabdulaziz628@gmail.com>
2023-04-28 13:35:32 +0300

    Realtime

--- app/src/main/java/com/example/firebase2risley/MainActivity.kt
+++ app/src/main/java/com/example/firebase2risley/MainActivity.kt
@@ -2,10 +2,85 @@ package com.example.firebase2risley
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var edtcarmake:EditText
+    lateinit var edtcarmodel:EditText
+    lateinit var edtcarprice:EditText
+    lateinit var btncarphoto:Button
+    lateinit var btncardata:Button
+    lateinit var btnuploadedcars:Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edtcarmake = findViewById(R.id.edtcarmake)
+        edtcarmodel = findViewById(R.id.edtcarmodel)
+        edtcarprice = findViewById(R.id.edtcarprice)
+        btncarphoto = findViewById(R.id.btncarphoto)
+        btncardata = findViewById(R.id.btncardata)
+        btnuploadedcars = findViewById(R.id.btnuploadedcars)
+
+        var database = FirebaseDatabase.getInstance()
+        var databaseref = database.getReference("Car")
+
+
+        btncarphoto.setOnClickListener {
+
+
+        }
+
+        btncardata.setOnClickListener {
+
+            var carmake = edtcarmake.text.toString().trim()
+            var carmodel = edtcarmodel.text.toString().trim()
+            var carprice = edtcarprice.text.toString().trim()
+
+
+
+            if (carmake.isEmpty() || carmodel.isEmpty() || carprice.isEmpty()){
+                Toast.makeText(this, "Cannot Submit Empty Field", Toast.LENGTH_SHORT).show()
+            }else{
+
+                //saving info to firebase db
+
+
+                var usercar = car(carmake,carmake,carprice)
+
+                var ref = FirebaseDatabase.getInstance().getReference().child("cars")
+
+                ref.setValue(usercar).addOnCompleteListener{
+
+                    if (it.isSuccessful){
+                        Toast.makeText(this,"Car Data Uploaded Successfully",
+                            Toast.LENGTH_LONG).show()
+                    }else{
+                        Toast.makeText(this,"Failed tO Save Car Info",
+                            Toast.LENGTH_LONG).show()
+                    }
+
+                }
+
+
+            }
+
+
+        }
+
+        btnuploadedcars.setOnClickListener {
+
+
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/firebase2risley/car.kt
@@ -0,0 +1,20 @@
+package com.example.firebase2risley
+
+class car {
+
+    var car_make:String? = null
+    var car_model:String? = null
+    var car_price:String? = null
+
+
+    constructor(car_make:String, car_Model:String, car_price:String){
+        this.car_make = car_make
+        this.car_model = car_Model
+        this.car_price = car_price
+
+    }
+
+
+
+
+}
